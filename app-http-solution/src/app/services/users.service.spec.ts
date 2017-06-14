af3478ed490c6bda9d1bd604659f9bd7ebf511d2
Gerard Sans <gerard.sans@gmail.com>
2017-06-14 10:53:43 +0100

    Added tests

--- app-http-solution/src/app/services/users.service.spec.ts
+++ app-http-solution/src/app/services/users.service.spec.ts
@@ -1,4 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { async } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { Http, HttpModule, XHRBackend, Response, BaseRequestOptions, ResponseOptions } from '@angular/http';
 
 import { UsersService } from './users.service';
 
@@ -12,4 +15,50 @@ describe('UsersService', () => {
   it('should be created', inject([UsersService], (service: UsersService) => {
     expect(service).toBeTruthy();
   }));
+});
+
+fdescribe('MockBackend: UsersService', () => {
+  let mockbackend, service;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      BaseRequestOptions,
+      MockBackend,
+      { provide: Http,
+        useFactory: (backend, options) => new Http(backend, options),
+        deps: [MockBackend, BaseRequestOptions] },
+      UsersService
+    ]
+  }));
+  
+  beforeEach(inject([MockBackend, UsersService], (m, s) => {
+    mockbackend = m;
+    service = s;
+  }))
+
+  it('should return mocked response', done => {
+    //setup mock response
+    let response = { users: [{
+        "id": 67,
+        "username": "Chris",
+        "roles": [
+          "user"
+        ],
+        "superuser": false
+      }]
+    };
+    mockbackend.connections.subscribe(connection => {
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: JSON.stringify(response),
+        status: 200
+      })));
+    });
+
+    //check expectations
+    service.get().subscribe(users => {
+      expect(users[0].username).toBe("Chris");
+      expect(users.length).toBe(1);
+      done();
+    });
+  });
 });
--- app-modules-solution/src/app/shared/greeter/greeter.component.spec.ts
+++ app-modules-solution/src/app/shared/greeter/greeter.component.spec.ts
@@ -1,10 +1,14 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 import { GreeterComponent } from './greeter.component';
 
-describe('GreeterComponent', () => {
-  let component: GreeterComponent;
+fdescribe('GreeterComponent', () => {
+  let greeter: GreeterComponent;
   let fixture: ComponentFixture<GreeterComponent>;
+  let element: any;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,11 +19,45 @@ describe('GreeterComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GreeterComponent);
-    component = fixture.componentInstance;
+    greeter = fixture.componentInstance;
+    element = fixture.nativeElement;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
-    expect(component).toBeTruthy();
+    expect(greeter).toBeTruthy();
+  });
+
+  //specs
+  it('should render `Hello World!` (async)', async(() => {
+    greeter.name = 'World';
+    //trigger change detection
+    fixture.detectChanges();
+    fixture.whenStable().then(() => { 
+      expect(element.querySelector('h1').innerText).toBe('Hello World!');
+      expect(de.query(By.css('h1')).nativeElement.innerText).toBe('Hello World!');
+    });
+  }));
+  
+  it('should render `Hello World!` (fakeAsync/tick)', fakeAsync(() => {
+    greeter.name = 'World';
+    //trigger change detection
+    fixture.detectChanges();
+    //execute all pending asynchronous calls
+    tick();
+    expect(element.querySelector('h1').innerText).toBe('Hello World!');
+    expect(de.query(By.css('h1')).nativeElement.innerText).toBe('Hello World!');
+  }));
+  
+  it('should render `Hello World!` (done)', done => {
+    greeter.name = 'World';
+    //trigger change detection
+    fixture.detectChanges();
+    fixture.whenStable().then(() => { 
+      expect(element.querySelector('h1').innerText).toBe('Hello World!');
+      expect(de.query(By.css('h1')).nativeElement.innerText).toBe('Hello World!');
+      done();
+    });
   });
 });
